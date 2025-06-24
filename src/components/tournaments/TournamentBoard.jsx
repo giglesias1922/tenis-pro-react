6f9adfbd82b96678d821996ac398520ef69e6947
Gustavo <giglesias@oriontech.com.ar>
2025-06-24 11:44:24 -0300

    tournament boad

--- src/components/tournaments/TournamentBoard.jsx
+++ src/components/tournaments/TournamentBoard.jsx
@@ -1,50 +1,52 @@
 import React, { useEffect, useState } from "react";
 import { getTournamentsBoard } from "../../services/tournamentsService";
-import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
-import CardMedia from "@mui/material/CardMedia";
-import Typography from "@mui/material/Typography";
-import Button from "@mui/material/Button";
-import CardActionArea from "@mui/material/CardActionArea";
-import CardActions from "@mui/material/CardActions";
+import {
+  Grid,
+  Card,
+  CardMedia,
+  CardActions,
+  Button,
+  Dialog,
+  DialogTitle,
+  DialogContent,
+  Container,
+  Paper,
+} from "@mui/material";
 
 export const TournamentBoard = () => {
-  const [Data, setData] = useState([]);
+  const [data, setData] = useState([]);
 
-  useEffect(async () => {
-    var response = await getTournamentsBoard();
-    console.log("response", response);
-    setData(response);
-  });
+  useEffect(() => {
+    var response = getTournamentsBoard().then((res) => {
+      setData(res);
+      console.log("data", res);
+    });
+  }, []);
 
   return (
-    {
-        data.map(element => (
-        
-    
-    <Card sx={{ maxWidth: 345 }}>
-      <CardActionArea>
-        <CardMedia
-          component="img"
-          height="140"
-          image="/static/images/cards/contemplative-reptile.jpg"
-          alt="green iguana"
-        />
-        <CardContent>
-          <Typography gutterBottom variant="h5" component="div">
-            Lizard
-          </Typography>
-          <Typography variant="body2" sx={{ color: "text.secondary" }}>
-            Lizards are a widespread group of squamate reptiles, with over 6,000
-            species, ranging across all continents except Antarctica
-          </Typography>
-        </CardContent>
-      </CardActionArea>
-      <CardActions>
-        <Button size="small" color="primary">
-          Share
-        </Button>
-      </CardActions>
-    </Card>
-))});
+    <Grid container spacing={2} elevation={3}>
+      {data.map((tournament) => (
+        <Grid item xs={12} sm={6} mt={5} md={4} key={tournament.id}>
+          <Card>
+            <CardMedia
+              component="img"
+              height="100%"
+              image={tournament.image}
+              alt="Tournament banner"
+            />
+            <CardActions>
+              <Button
+                size="small"
+                color="primary"
+                onClick={() => handleOpen(tournament)}
+              >
+                Inscribirse
+              </Button>
+            </CardActions>
+          </Card>
+        </Grid>
+      ))}
+    </Grid>
+  );
 };
